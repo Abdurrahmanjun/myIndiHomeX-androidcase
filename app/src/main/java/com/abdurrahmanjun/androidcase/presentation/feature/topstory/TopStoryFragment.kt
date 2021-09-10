8645a87e8f73f2f06ab18b248574d28e017e9cbd
Abdurrahmanjun <abdurrahmanjun@gmail.com>
2021-09-10 10:29:48 +0700

    ready to load asynchronously

--- app/src/main/java/com/abdurrahmanjun/androidcase/presentation/feature/topstory/TopStoryFragment.kt
+++ app/src/main/java/com/abdurrahmanjun/androidcase/presentation/feature/topstory/TopStoryFragment.kt
@@ -1,5 +1,35 @@
 package com.abdurrahmanjun.androidcase.presentation.feature.topstory
 
-class TopStoryActivity {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.abdurrahmanjun.androidcase.R
+import com.abdurrahmanjun.androidcase.presentation.feature.storydetails.StoryDetailsFragment
 
+class TopStoryFragment : Fragment(), View.OnClickListener {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_story_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onClick(v: View) {
+//        if (v.id == R.id.btn_category) {
+//            val mStoryDetailsFragment = StoryDetailsFragment()
+//            val mFragmentManager = fragmentManager
+//            mFragmentManager?.beginTransaction()?.apply {
+//                replace(R.id.frame_container, mStoryDetailsFragment, StoryDetailsFragment::class.java.simpleName)
+//                addToBackStack(null)
+//                commit()
+//            }
+//        }
+    }
 }
